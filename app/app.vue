6eb2264931f151ba6e3237dd52419d4b64c52bd7
FelixH3r <f3l1x.hermanutz@gmail.com>
2026-05-03 21:43:17 +0200

    feat: add og-image for social media previews

--- app/app.vue
+++ app/app.vue
@@ -6,4 +6,10 @@
 </template>
 <script setup>
 import { Analytics } from "@vercel/analytics/nuxt";
+
+useSeoMeta({
+  ogImage: "/og-image-black-on-white-1200x630.png",
+  twitterImage: "/og-image-black-on-white-1200x630.png",
+  twitterCard: "summary_large_image",
+});
 </script>
